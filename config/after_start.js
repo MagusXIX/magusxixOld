8f30092596838ae9313cd65bae5eecfc06fe78c6
MagusXIX <magusxix@gmail.com>
2013-12-11 15:55:06 -0800

    Bare bones chat up and running.
    Need to work on users/dice roller next.

--- config/after_start.js
+++ config/after_start.js
@@ -1,8 +1,12 @@
 geddy.io = require('socket.io').listen(geddy.server, {'log level': 0});
 
 geddy.io.sockets.on('connection', function (socket) {
-  socket.emit('news', {hello: 'world'});
-  socket.on('my other event', function (data) {
+  socket.emit('establishing', {established: 'true'});
+  socket.on('established', function (data) {
     console.log(data);
+  })
+  socket.on('sentMessage', function (data) {
+  	console.log(data);
+  	socket.broadcast.emit('receivedMessage', data)
   })
 })
--- public/js/controllers/chat.js
+++ public/js/controllers/chat.js
@@ -2,9 +2,63 @@ var geddy = {};
 window.geddy = geddy || {};
 geddy.io = require('socket.io-client');
 geddy.socket = geddy.io.connect('/');
-console.log(geddy.socket);
 
-geddy.socket.on('news', function (data) {
+$('html').css("height", "100%");
+$('html').css("width", "100%");
+$('body').css("height", "100%");
+$('body').css("width", "100%");
+$('#container').css("height", "100%");
+$('#container').css("width", "100%");
+
+var messageCount = 0;
+
+$('#messageFormInput').keypress(function (event) {
+
+  if (event.which == 13) {
+
+    event.preventDefault();
+    geddy.socket.emit('sentMessage', $('#messageFormInput').val());
+
+    $('#sentMessages').append("<div class='sentMessage' style='margin-bottom: 1px; word-wrap: break-word'>" + "<p class='sentMessageText'>" + $('#messageFormInput').val() + "</p></div>");
+    $('#messageFormInput').val('');
+
+    messageCount++;
+
+    if ((messageCount * 20) > $("#sentMessages").height()) {
+      $("#sentMessages").scrollTop(messageCount * 20);
+    }
+
+    if ($('#sentMessages').width() > $('#mainChatCan').width()) {
+      $('#sentMessages').width = $('#mainChatCan').width();
+    }
+
+  }
+
+});
+
+$('#messageFormSubmit').click(function (event) {
+
+  event.preventDefault();
+  geddy.socket.emit('sentMessage', $('#messageFormInput').val());
+
+  $('#sentMessages').append("<div class='sentMessage' style='margin-bottom: 1px; word-wrap: break-word'>" + $('#messageFormInput').val() + "</div>");
+  $('#messageFormInput').val('');
+  $('#messageFormInput').focus();
+
+  messageCount++;
+
+  if ((messageCount * 20) > $("#sentMessages").height()) {
+    $("#sentMessages").scrollTop(messageCount * 20);
+  }
+
+})
+
+geddy.socket.on('receivedMessage', function (data) {
   console.log(data);
-  geddy.socket.emit('my other event', {my: 'data'});
+  $('#sentMessages').append("<div class='sentMessage'>" + data + "</div>");
+})
+
+geddy.socket.on('establishing', function (data) {
+  console.log(data);
+  geddy.socket.emit('established', {established: 'true'});
 });
