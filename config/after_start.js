201c7d27e977c6b885a743ac7e95dca665d79390
MagusXIX <magusxix@gmail.com>
2013-12-11 18:44:25 -0800

    Functioning dice roller.
    Just needs users now, and then polish.

--- config/after_start.js
+++ config/after_start.js
@@ -1,12 +1,19 @@
 geddy.io = require('socket.io').listen(geddy.server, {'log level': 0});
 
 geddy.io.sockets.on('connection', function (socket) {
+
   socket.emit('establishing', {established: 'true'});
   socket.on('established', function (data) {
     console.log(data);
   })
+
   socket.on('sentMessage', function (data) {
   	console.log(data);
-  	socket.broadcast.emit('receivedMessage', data)
+  	socket.broadcast.emit('receivedMessage', data);
   })
+
+  socket.on('diceRoll', function (data) {
+  	socket.broadcast.emit('diceRollEmit', data);
+  })
+
 })
--- public/js/controllers/chat.js
+++ public/js/controllers/chat.js
@@ -10,6 +10,8 @@ $('body').css("width", "100%");
 $('#container').css("height", "100%");
 $('#container').css("width", "100%");
 
+
+//MESSAGE HANDLERS
 var messageCount = 0;
 
 $('#messageFormInput').keypress(function (event) {
@@ -57,6 +59,60 @@ geddy.socket.on('receivedMessage', function (data) {
   console.log(data);
   $('#sentMessages').append("<div class='sentMessage'>" + data + "</div>");
 })
+
+
+//DICE HANDLERS
+$('#actionInput').keypress(function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+  }
+})
+
+$('#diceNumber').keypress(function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+  }
+})
+
+$('#diceSides').keypress(function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+  }
+})
+
+$('#diceFormSubmit').click(function (event) {
+
+  event.preventDefault();
+
+  var rollTheDice = function (callback) {
+    var rollResults = [];
+
+    for (var i = 1; i <= $('#diceNumber').val(); i++) {
+      var result = Math.floor((Math.random() * $('#diceSides').val()) + 1);
+      rollResults.push(result);
+    }
+
+    callback(rollResults);
+  }
+
+  var displayResults = function (rollResults) {
+
+    var resultsMessage = '';
+    resultsMessage = 'You rolled ' + (rollResults.length + 1) + ', ' + $('#diceSides').val() + ' sided dice to ' + $('#actionInput').val() + ' and the results were: ' + rollResults;
+
+    $('#sentMessages').append("<div class='sentMessage' style='margin-bottom: 1px; word-wrap: break-word'>" + resultsMessage + "</div>");
+    geddy.socket.emit('diceRoll', resultsMessage);
+
+  }
+
+  rollTheDice(displayResults);
+
+});
+
+geddy.socket.on('diceRollEmit', function (data) {
+  console.log(data);
+  $('#sentMessages').append("<div class='sentMessage'>" + data + "</div>");
+});
 
 geddy.socket.on('establishing', function (data) {
   console.log(data);
